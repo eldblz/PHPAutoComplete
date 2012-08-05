50c0e3e365dc8b0e697b1b6103278898410dc4f9
eldblz <eldiabloz@gmail.com>
2012-08-05 22:58:24 +0200

    Better example and added exampleWithSecurity.php

--- example/exampleWithSecurity.php
+++ example/exampleWithSecurity.php
@@ -8,6 +8,9 @@ $Database = 'test';
 $Username = 'root';
 $Password = '';
 
+$AllowedTables = array('cities', 'countries');
+$AllowedColumns = array('city', 'country');
+
 try
 {
 	
@@ -19,11 +22,16 @@ try
 		$column = $_GET['col'];
 		$table = $_GET['tbl'];
 		
-		//.Create new obj
-		$myAutoCompleteHelper = new AutoCompleteHelper($Hostname, $Username, $Password, $Database);
 		
-		//.Query database and return json encoded results for jqeury autocomplete
-		echo $myAutoCompleteHelper->AutoComplete($table, $column, $term);
+		//Check if required table and column are allowed
+		if( in_array($table, $AllowedTables) && in_array($column, $AllowedColumns) )
+		{
+			//.Create new obj
+			$myAutoCompleteHelper = new AutoCompleteHelper($Hostname, $Username, $Password, $Database);
+			
+			//.Query database and return json encoded results for jqeury autocomplete
+			echo $myAutoCompleteHelper->AutoComplete($table, $column, $term);
+		}
 		
 	}
 }
